869af8bf456f3dfea02dfa60785ac998d882f3e9
Levchenko Aleksandr <madlion@mail.ru>
2024-07-15 02:18:29 +0700

    Changes to be committed: 	modified:   recipes/admin.py 	modified:   recipes/constants.py

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from .constants import LIST_PER_PAGE
 from .models import (Favorite, Ingredient, IngredientInRecipe, Recipe,
                      ShoppingCart, Tag)
 
@@ -14,7 +15,8 @@ class IngredientAdmin(admin.ModelAdmin):
         'name',
         'measurement_unit',
     )
-    list_per_page = 15
+    search_fields = ('name',)
+    list_per_page = LIST_PER_PAGE
     list_display_links = ('name',)
     inlines = (IngredientInline,)
 
@@ -36,13 +38,19 @@ class ShoppingCartAdmin(admin.ModelAdmin):
 
 
 class RecipeAdmin(admin.ModelAdmin):
-
     list_display = (
         'name',
         'author',
         'pub_date',
+        'count_in_favorite',
     )
-    list_per_page = 15
+
+    def count_in_favorite(self, obj):
+        return Favorite.objects.filter(recipes=obj).count()
+    count_in_favorite.short_description = 'раз в избранном'
+    search_fields = ('name', 'author',)
+    list_filter = ('tags',)
+    list_per_page = LIST_PER_PAGE
     list_display_links = ('name',)
     inlines = (IngredientInline,)
 
@@ -52,7 +60,7 @@ class TagAdmin(admin.ModelAdmin):
         'name',
         'slug',
     )
-    list_per_page = 15
+    list_per_page = LIST_PER_PAGE
     list_display_links = ('name',)
 
 
