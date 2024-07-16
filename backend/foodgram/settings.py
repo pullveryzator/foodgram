ccd9cdfb763df5cc41bd948c4a9ad7ef342ee29e
Levchenko Aleksandr <madlion@mail.ru>
2024-07-16 23:23:44 +0700

    Changes to be committed: 	modified:   backend/foodgram/settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -2,7 +2,6 @@ import os
 from pathlib import Path
 
 from .constants import DEFAULT_PAGE_SIZE
-from .debug_flag import debug_flag
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
